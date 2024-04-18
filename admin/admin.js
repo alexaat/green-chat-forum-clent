c3a9af212dabf1534f3f2161fb7e263c7a18b200
aliakseividaseu <alexaat@yahoo.co.uk>
2024-04-18 21:15:07 +0100

    delete post

--- admin/admin.js
+++ admin/admin.js
@@ -20,7 +20,11 @@ const renderAdminPage = () => {
     }
     const html = `
     <h1> Welcome Admin</h1>
-    <div id='users-panel'></div>    `
+    <div class='admin-page-container'>
+    <div id='users-panel'></div>
+    <div id='posts-panel'></div>
+    </div>
+    `;
     document.body.innerHTML = html;
     //Add Click listeners
     const usersPanel = document.querySelector('#users-panel');
@@ -31,9 +35,8 @@ const renderAdminPage = () => {
         }      
     });
 
-    //Get users
-    
-    const endpoint = host+"admin/users?" + new URLSearchParams({session_id});
+    //Get users    
+    let endpoint = host+"admin/users?" + new URLSearchParams({session_id});
     let headers = new Headers();
     headers.append('Accept', 'application/json');
     fetch(
@@ -53,6 +56,31 @@ const renderAdminPage = () => {
         }
         if(data.payload) {
             data.payload.forEach(user => renderUserComponent(user));
+        }
+    })
+    .catch(err => console.log(err))
+
+    //Get Posts
+    endpoint = host+"admin/posts?" + new URLSearchParams({session_id});
+    headers = new Headers();
+    headers.append('Accept', 'application/json');
+    fetch(
+        endpoint,
+        {method: 'GET', headers: headers}
+    )
+    .then(response => response.json())
+    .then(data => {
+        console.log('data posts',data)
+        if(data.error) {
+            if(data.error.type === AUTHORIZATION){
+                renderSignUpPage()
+            }else {
+                throw new Error(data.error)
+            }
+            return;            
+        }
+        if(data.payload) {
+            data.payload.forEach(post => renderPostComponent(post));
         }
     })
     .catch(err => console.log(err))
@@ -113,7 +141,6 @@ function showSignUpError(error) {
 function renderUserComponent(user) {
 
     const usersPanel = document.querySelector('#users-panel');
-
     const html = `
         <div class='user-container' data-id='${user.id}'>
             <div class='user-content'>
@@ -123,9 +150,30 @@ function renderUserComponent(user) {
         </div>
     `;
 
-    usersPanel.innerHTML = html;
-   
+    usersPanel.innerHTML += html;  
 
+}
+
+function renderPostComponent(post) {
+    const postsPanel = document.querySelector('#posts-panel');
+
+    const html = `
+        <div class='post-container' data-id='${post.id}'>
+            <div class='post-header'>
+                <div>
+                    ${post.nick_name}
+                    <br>
+                    ${post.date}
+                </div>                
+                <input type='button' value='Delete' class='delete-post-button'/>            
+            </div>
+            <div class='post-content'>             
+                ${post.content}               
+            </div>        
+        </div>
+    `;
+
+    postsPanel.innerHTML += html;
 }
 
 function deleteUser(id) { 
@@ -136,6 +184,25 @@ function deleteUser(id) {
         return;
     }
     const endpoint = host+`admin/users/${id}?` + new URLSearchParams({session_id});
+    let headers = new Headers();
+    headers.append('Accept', 'application/json');
+    fetch(
+        endpoint, {
+            method: 'DELETE',
+            headers: headers}
+        )
+    .then(resp => resp.json())
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+}
+
+function deletePost(id){
+    let session_id = getCookie(ADMIN_JSESSIONID);
+    if (!session_id) {
+        renderSignUpPage();
+        return;
+    }
+    const endpoint = host+`admin/posts/${id}?` + new URLSearchParams({session_id});
     let headers = new Headers();
     headers.append('Accept', 'application/json');
     fetch(
